0eea8e8fd92028d6ea03900e6d2559205da4e538
Michał Sośnicki <sosnicki.michal@gmail.com>
2022-12-24 04:39:42 +0100

    add solution to problem 22 part 2 WIP

--- 22_monkey_map/main.py
+++ 22_monkey_map/main.py
@@ -1,7 +1,13 @@
+import collections
 from dataclasses import dataclass
 import fileinput
+import math
+import numpy as np
 import re
-from typing import Dict, List, Protocol
+
+class Rotation:
+    RIGHT = 'R'
+    LEFT = 'L'
 
 
 class Field:
@@ -38,102 +44,199 @@ class Field:
         return '\n'.join(lines)
 
 
-class Direction:
-    RIGHT = 0
-    DOWN = 1
-    LEFT = 2
-    UP = 3
-
-    @classmethod
-    def horizontal(cls, val):
-        return val in (cls.RIGHT, cls.LEFT)
-
-
-@dataclass
 class Position:
-    y: int
-    x: int
-    direction: int
+    ROTATION_CW = np.array([[0, 1], [-1, 0]])
+    ROTATION_CCW = np.array([[0, -1], [1, 0]])
+
+    def __init__(self, coords, direction, side):
+        self.coords = coords
+        self.direction = direction
+        self.side = side
+
+    @staticmethod
+    def initial(x, y):
+        return Position(
+            coords=np.array([x, y, -1]),    # one is = -1 or size
+            direction=np.array([1, 0, 0]),  # e.g. [0, 1, 0] or [0, -1, 0]
+            side=np.array([0, 0, 1])
+        )
+
+    def copy(self):
+        return Position(
+            coords=self.coords.copy(),
+            direction=self.direction.copy(),
+            side=self.side.copy()
+        )
+
+    def __repr__(self):
+        return f'Position(coords={self.coords}, direction={self.direction}, side={self.side})'
+
+    def side_ix(self):
+        return (np.flatnonzero(self.side)[0] + 1) * self.side.sum()
 
     def rotate(self, rotation, times=1):
-        if rotation == 'R':
-            self.direction = (self.direction + times) % 4
-        elif rotation == 'L':
-            self.direction = (self.direction - times) % 4
-        else:
-            raise ValueError(f'unknown rotation {rotation}')
+        if times < 0:
+            times = -times
+            rotation = Rotation.LEFT if rotation == Rotation.RIGHT else Rotation.RIGHT
 
-    def step(self, steps=1):
-        if self.direction == Direction.RIGHT:
-            self.x += steps
-        elif self.direction == Direction.LEFT:
-            self.x -= steps
-        elif self.direction == Direction.DOWN:
-            self.y += steps
-        elif self.direction == Direction.UP:
-            self.y -= steps
+        print(f'rotation {self} {rotation} {times} times')
+        if (rotation == Rotation.RIGHT) == (self.side[0] == 1 or self.side[1] == -1 or self.side[2] == 1):
+            # y side is inverted bc we should rotate axes in order [z, x], not [x, z]
+            rotation_m = self.ROTATION_CW
         else:
-            raise ValueError(f'invalid direction {self.direction}')
+            rotation_m = self.ROTATION_CCW
+
+        for i in range(times):
+            self.direction[self.side == 0] = self.direction[self.side == 0] @ rotation_m
+        print(f'rotated {self}')
+
+    def step(self, size, times=1):
+        print(f'move {self}')
+        for i in range(times):
+            self.coords[:] += self.direction
+            self.clip_cube(size)
+        print(f'moved {self}')
+
+    def clip_cube(self, size):
+        on_side = self.coords[self.side == 0]
+        if (0 <= on_side).all() and (on_side < size).all():
+            return
+        print(f'cut from {self}')
+        # np. p[4 0 -1] d[1 0 0] s[0 0 1] -> p[4 0 0] d[0 0 -1] s[-1 0 0]
+        # np. p[-1 0 -1] d[-1 0 0] s[0 0 1] -> p[-1 0 0] d[0 0 -1] s[1 0 0]
+        # np. p[0 4 -1] d[0 1 0] s[0 0 1] -> p[0 4 0] d[0 0 -1] s[0 -1 0]
+        self.coords[:] += self.side
+        self.direction = self.side
+        self.side = (self.coords == size).astype(int) * -1 \
+                  + (self.coords == -1).astype(int) 
+        print(f'cut to {self}')
+
 
 @dataclass
-class Board:
-    def __init__(self, fields):
+class RegionMapping:
+    texture_y: int
+    texture_x: int
+    cube_position: Position
+    #y_to_coord_a: np.ndarray
+    #y_to_coord_b: np.ndarray
+    #x_to_coord_a: np.ndarray
+    #x_to_coord_b: np.ndarray
+
+
+class Texture:
+    def __init__(self, fields, size):
         self.fields = fields
 
-        height = len(fields) + 1
-        width = max(len(row) for row in fields) + 1
-        self.lft_edge = [width] * height
-        self.rht_edge = [0] * height
-        self.top_edge = [height] * width
-        self.bot_edge = [0] * width
+        height = len(fields)
+        width = max(len(row) for row in fields)
+        grid_h = height // size
+        grid_w = width // size
+        print(size, height, width, grid_h, grid_w)
 
-        for y, row in enumerate(self.fields):
-            for x, cell in enumerate(row):
+        first_region = None
+        grid = [[0] * grid_w for y in range(grid_h)]
+        for yi in range(grid_h):
+            for xi in range(grid_w):
+                if xi * size >= len(fields[yi * size]):
+                    continue
+                cell = fields[yi * size][xi * size]
                 if cell != Field.VOID:
-                    self.lft_edge[y] = min(self.lft_edge[y], x)
-                    self.rht_edge[y] = max(self.rht_edge[y], x)
-                    self.top_edge[x] = min(self.top_edge[x], y)
-                    self.bot_edge[x] = max(self.bot_edge[x], y)
+                    grid[yi][xi] = 1
+                    if first_region is None:
+                        first_region = (yi, xi)
 
-    def first_position(self, direction=Direction.RIGHT):
-        for y, row in enumerate(self.fields):
-            for x, cell in enumerate(row):
-                if cell == Field.FLOOR:
-                    return Position(y=y, x=x, direction=direction)
+        mappings = {}
+        queue = collections.deque()
+        dydx_to_r_rotations = {
+            (0, 1): 0, (1, 0): 1, (0, -1): 2, (-1, 0): 3,
+        }
+
+        def visit(yi, xi, p_yi, p_xi):
+            if grid[yi][xi] != 1:
+                return
+
+            if p_yi is None:
+                position = Position.initial(0, 0)
+                mapping = RegionMapping(
+                    texture_x=xi * size, texture_y=yi * size, 
+                    cube_position=position,
+                    #y_to_coord_a=np.array([0, 1, 0]),
+                    #y_to_coord_b=np.array([0, 0, 0]),
+                    #x_to_coord_a=np.array([1, 0, 0]),
+                    #x_to_coord_b=np.array([0, 0, 0]),
+                )
+            else:
+                p_mapping = mappings[(p_yi, p_xi)]
+
+                dy, dx = yi - p_yi, xi - p_xi
+                r_rotations = dydx_to_r_rotations[(dy, dx)]
+                position = p_mapping.cube_position.copy()
+                position.rotate(Rotation.RIGHT, times=r_rotations)
+                position.step(times=size, size=size)
+                position.rotate(Rotation.RIGHT, times=-r_rotations)
+
+                mapping = RegionMapping(
+                    texture_x=xi * size, texture_y=yi * size, 
+                    cube_position=position,
+                )
+
+            mappings[(yi, xi)] = mapping
+            grid[yi][xi] = 2
+            queue.append((yi, xi))
+
+        visit(first_region[0], first_region[1], p_yi=None, p_xi=None)
+        while queue:
+            y, x = queue.popleft()
+            if y > 0: 
+                visit(y - 1, x, p_yi=y, p_xi=x)
+            if y < grid_h - 1: 
+                visit(y + 1, x, p_yi=y, p_xi=x)
+            if x > 0: 
+                visit(y, x - 1, p_yi=y, p_xi=x)
+            if x < grid_w - 1: 
+                visit(y, x + 1, p_yi=y, p_xi=x)
+
+        side_ix_to_mapping = {}
+        for mapping in mappings.values():
+            side_ix = mapping.cube_position.side_ix()
+            side_ix_to_mapping[side_ix] = mapping
+        print(side_ix_to_mapping)
+        self.side_ix_to_mapping = side_ix_to_mapping
+
+    def cube_to_grid_coords(self, x, y, z):
+        return 0, 0
+
+    def cube_to_grid_direction(self, direction):
+        return 0
+
+    def get_field(self, x, y, z):
+        grid_y, grid_x = self.cube_to_grid_coords(x, y, z)
+        return self.fields[grid_y][grid_x]
+
+
+class Board:
+    def __init__(self, size, texture):
+        self.size = size
+        self.texture = texture
+
+    def first_position(self):
+        for y in range(size):
+            for x in range(size):
+                field = self.texture.get_field(y=y, x=x, z=0)
+                if field == Field.FLOOR:
+                    return Position.initial(y=y, x=x)
 
     def move(self, position: Position, steps: int):
         for step in range(steps):
-            position.step(steps=1)
-            self.clip_plane(position)
-            field = self.fields[position.y][position.x]
+            position.step(times=1, size=self.size)
+            field = self.texture.get_field(*position.coords)
             if field == Field.WALL:
-                print(f'HIT A WALL at {position}')
-                position.step(steps=-1)
-                self.clip_plane(position)
+                print(f'wall! {position}')
+                position.step(times=-1, size=self.size)
                 break
 
-    def clip_plane(self, position: Position):
-        print(f'position {position}', end=' ')
-        if Direction.horizontal(position.direction):
-            self.clip_x(position)
-        else:
-            self.clip_y(position)
-        print(f'clipped to {position}')
 
-    def clip_y(self, position: Position):
-        x = position.x
-        min_y, max_y = self.top_edge[x], self.bot_edge[x]
-        span_y = max_y - min_y + 1
-        position.y = (position.y - min_y) % span_y + min_y
-
-    def clip_x(self, position: Position):
-        y = position.y
-        min_x, max_x = self.lft_edge[y], self.rht_edge[y]
-        span_x = max_x - min_x + 1
-        position.x = (position.x - min_x) % span_x + min_x
-
-
-def read_input(alt_mode=True):
+def read_input():
     fields = []
 
     read_phase = 0
@@ -147,12 +250,22 @@ def read_input(alt_mode=True):
         else:
             instructions = re.split("([RL])", line)
 
-    board = Board(fields)
-    return board, instructions
+    return fields, instructions
+
+
+def guess_size(fields):
+    height = len(fields)
+    width = max(len(row) for row in fields)
+    size = math.gcd(height, width)
+    grid_h = height // size
+    grid_w = width // size
+    if grid_h * grid_w < 8:
+        size /= 2
+    return size
 
 
 def simulate(board, position, instructions):
-    print(f'initial position {position}')
+    print(f'init {position}')
     for instruction in instructions:
         print(f'executing {instruction}')
         if instruction.isnumeric():
@@ -162,19 +275,27 @@ def simulate(board, position, instructions):
             position.rotate(instruction)
         else:
             raise ValueError(f'unknown instruction {instruction}')
-        print(f'position {position}')
+        print(position)
 
     return position
 
 
 if __name__ == '__main__':
-    board, instructions = read_input()
-    print(Field.show(board.fields))
+    fields, instructions = read_input()
+    print(Field.show(fields))
     print(instructions)
+    size = guess_size(fields)
+    print('size', size)
 
+    texture = Texture(fields=fields, size=size)
+    board = Board(size=size, texture=texture)
+
+    raise ValueError()
     position = board.first_position()
     position = simulate(board, position, instructions)
-    print(position)
-    print(1000 * (position.y + 1) + 4 * (position.x + 1) + position.direction)
+
+    grid_y, grid_x = texture.cube_to_grid_coords(*position.coords)
+    grid_direction = texture.cube_to_grid_direction(position.direction)
+    print(1000 * (grid_y + 1) + 4 * (grid_x + 1) + grid_direction)
 
 
